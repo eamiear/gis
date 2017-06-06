8e2b22b931fde118ec1c446c8fa48cb158d665bb
eamiear <justcall_me_k@163.com>
2017-06-06 20:53:12 +0800

    菜单项格式

--- sample/static/js/items.js
+++ sample/static/js/items.js
@@ -1,81 +1,89 @@
 /**
  * Created by K on 2017/6/2.
  */
+
+/**
+ * 菜单
+ * header： 一级菜单名称
+ * children： 二级菜单集合
+ * title： 二级菜单名称
+ * href: demo路径
+ */
 var menuDatas = [{
-        header: '地图示例',
-        //href:'',
-        children: [{
-                title: '地图展示',
-                href: 'views/demo/a1_2.htm'
-            },
-            {
-                title: '移动地图',
-                href: 'views/demo/a3_1.htm'
-            },
-            {
-              title: '缩放地图',
-              href: 'views/demo/a3_2.htm'
-            },
-          {
-            title: '设置地图显示范围',
-            href: 'views/demo/a1_7.htm'
-          },
-          {
-            title: '获取地图显示范围',
-            href: 'views/demo/a4_3.htm'
-          }
-        ]
-    },
-    {
-        header: '地图操作示例',
-        //href:'',
-        children: [{
-            title: '地图基本操作',
-            href: 'views/demo/e1_1.htm'
-        },{
-          title: '地图测距、测面操作',
-          href: 'views/demo/e1_2.htm'
-        }]
-    },
-    {
-        header: '地图控件示例',
-        //href:'',
-        children: [{
-            title: '添加/删除工具条、比例尺控件',
-            href: 'views/demo/b0_2.htm'
-        },{
-          title: '地图图层切换',
-          href: 'views/demo/b0_3.htm'
-        }]
-    },
-    {
-        header: '覆盖物示例',
-        //href:'',
-        children: [{
-            title: '添加/删除覆盖物',
-            href: 'views/demo/c1_1.htm'
-        },{
-          title: '手动绘制/删除覆盖物',
-          href: 'views/demo/c1_2.htm'
-        }]
-    },
-    {
-        header: '信息窗口示例',
-        //href:'',
-        children: [{
-            title: '添加纯文字的信息窗口',
-            href: 'views/demo/d0_1.htm'
-        },{
-          title: '添加自定义属性信息窗口',
-          href: 'views/demo/d0_2.htm'
-        },{
-          title: '添加图文组合的信息窗口',
-          href: 'views/demo/d0_6.htm'
-        },{
-          title: '添加视频展示信息窗口',
-          href: 'views/demo/d0_4.htm'
-        }]
-    },
+    header: '地图示例',
+    //href:'',
+    children: [{
+          title: '地图展示',
+          href: 'views/demo/a1_2.htm'
+      },
+      {
+          title: '移动地图',
+          href: 'views/demo/a3_1.htm'
+      },
+      {
+        title: '缩放地图',
+        href: 'views/demo/a3_2.htm'
+      },
+      {
+        title: '设置地图显示范围',
+        href: 'views/demo/a1_7.htm'
+      },
+      {
+        title: '获取地图显示范围',
+        href: 'views/demo/a4_3.htm'
+      }
+    ]
+  },
+  {
+      header: '地图操作示例',
+      //href:'',
+      children: [{
+          title: '地图基本操作',
+          href: 'views/demo/e1_1.htm'
+      },{
+        title: '地图测距、测面操作',
+        href: 'views/demo/e1_2.htm'
+      }]
+  },
+  {
+      header: '地图控件示例',
+      //href:'',
+      children: [{
+          title: '添加/删除工具条、比例尺控件',
+          href: 'views/demo/b0_2.htm'
+      },{
+        title: '地图图层切换',
+        href: 'views/demo/b0_3.htm'
+      }]
+  },
+  {
+      header: '覆盖物示例',
+      //href:'',
+      children: [{
+          title: '添加/删除覆盖物',
+          href: 'views/demo/c1_1.htm'
+      },{
+        title: '手动绘制/删除覆盖物',
+        href: 'views/demo/c1_2.htm'
+      }]
+  },
+  {
+      header: '信息窗口示例',
+      //href:'',
+      children: [{
+          title: '添加纯文字的信息窗口',
+          href: 'views/demo/d0_1.htm'
+      },{
+        title: '添加自定义属性信息窗口',
+        href: 'views/demo/d0_2.htm'
+      },{
+        title: '添加图文组合的信息窗口',
+        href: 'views/demo/d0_6.htm'
+      },{
+        title: '添加视频展示信息窗口',
+        href: 'views/demo/d0_4.htm'
+      }]
+  },
   {
     header: '定位示例',
     //href:'',
